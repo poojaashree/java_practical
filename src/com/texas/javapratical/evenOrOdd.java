9e26dc7805ac28ee65703ff69825c2ea9659bd3a
PoojaShree <poojakaini0@gmail.com>
2025-08-30 09:11:08 +0545

    OOP basic commit

---
+++ src/com/texas/javapratical/evenOrOdd.java
@@ -0,0 +1,16 @@
+package com.texas.javapratical;
+
+import java.util.Scanner;
+
+public class evenOrOdd {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter any number: ");
+        int num = sc.nextInt();
+
+        if (num % 2 == 0)
+            System.out.println(num + " is Even.");
+        else
+            System.out.println(num + " is Odd.");
+    }
+}
